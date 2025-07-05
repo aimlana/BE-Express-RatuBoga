d9dd2b935738c671750216a8f60912fc4554e008
Muh. Salim Maulana <salim.yaklu@gmail.com>
2025-07-05 10:19:57 +0800

    feat: :sparkles: generate table QR code

---
+++ scripts/tableQrGenerator.js
@@ -0,0 +1,34 @@
+const QRCode = require('qrcode');
+const fs = require('fs');
+const path = require('path');
+
+const baseURL = 'http://localhost:5173/order/';
+const outputFolder = path.join(__dirname, '..', 'public', 'qrMeja');
+
+if (!fs.existsSync(outputFolder)) {
+  fs.mkdirSync(outputFolder);
+}
+
+for (let i = 1; i <= 5; i++) {
+  const url = `${baseURL}${i}`;
+  const filename = path.join(outputFolder, `meja-${i}.png`);
+
+  QRCode.toFile(
+    filename,
+    url,
+    {
+      errorCorrectionLevel: 'H',
+      margin: 2,
+      width: 300,
+    },
+    (err) => {
+      if (err) {
+        console.error(`Gagal buat QR untuk meja ${i}:`, err);
+      } else {
+        console.log(
+          `QR code untuk meja ${i} berhasil dibuat di: ${filename}`
+        );
+      }
+    }
+  );
+}
