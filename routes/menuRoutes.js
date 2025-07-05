fd75cfae14a57c79fb6881c5af34b96876aee146
Muh. Salim Maulana <salim.yaklu@gmail.com>
2025-07-05 08:49:20 +0800

    refactor(auth): :fire: remove authorizeRoles middleware

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const menuController = require('../controllers/menuController');
 const {
-  authenticateToken,
-  authorizeRoles,
+  authenticateToken
 } = require('../middlewares/authMiddleware');
 const { multerUpload } = require('../middlewares/uploads');
 
-const adminOnly = authorizeRoles([1]);
 
 // Public
 router.get('/', menuController.getAllMenus);
@@ -18,20 +16,17 @@ router.post(
   '/create',
   multerUpload.single('image'),
   authenticateToken,
-  adminOnly,
   menuController.createMenu
 );
 router.put(
   '/:id',
   multerUpload.single('image'),
   authenticateToken,
-  adminOnly,
   menuController.updateMenu
 );
 router.delete(
   '/:id',
   authenticateToken,
-  adminOnly,
   menuController.deleteMenu
 );
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,41 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
-const {
-  authenticateToken,
-  authorizeRoles,
-} = require('../middlewares/authMiddleware');
+const { getProfile, updateProfile } = require('../controllers/userController');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
-// Admin-only routes
-router.get(
-  '/',
-  authenticateToken,
-  authorizeRoles([1]),
-  userController.getAllUsers
-);
-router.get(
-  '/:uuid',
-  authenticateToken,
-  authorizeRoles([1]),
-  userController.getUserByUuid
-);
-router.post(
-  '/',
-  authenticateToken,
-  authorizeRoles([1]),
-  userController.createUser
-);
-router.put(
-  '/:uuid',
-  authenticateToken,
-  authorizeRoles([1]),
-  userController.updateUserByUuid
-);
-router.delete(
-  '/:uuid',
-  authenticateToken,
-  authorizeRoles([1]),
-  userController.deleteUserByUuid
-);
+router.get('/profile', authenticateToken, getProfile);
+router.put('/profile', authenticateToken, updateProfile);
 
 module.exports = router;
